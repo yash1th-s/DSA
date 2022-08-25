2815123ae0b8b6a0947a972287de0736805f3423
Yashwanth S <83501889+yash1th-s@users.noreply.github.com>
2022-08-25 17:12:29 +0530

    Create Diagonal Sum.java

---
+++ Diagonal Sum.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Diagonal_sum {
+
+    public static int diagonalSum(int matrix[][]){
+        int n = matrix.length, sum = 0;
+
+        for(int i=0;i<n;i++){
+          
+          //primary diagonal  
+            sum += matrix[i][i];
+
+          //secondary diagonal
+          /*if condition for duplicate sum in case of odd matrix 
+          (e.x:In case of 3x3 matrix, middle term gets added twice, 
+          to avoid that if add if condition) */
+            if(i != n-1-i)
+                //i+j = n-1
+                sum += matrix[i][n-1-i];
+        }
+    return sum;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int matrix[][] = new int[3][3];
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(diagonalSum(matrix));
+        sc.close();
+    }
+    
+}

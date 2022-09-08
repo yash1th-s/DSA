13943323fdda093df04c5627f230cc7a2e02e9fd
Yashwanth S <83501889+yash1th-s@users.noreply.github.com>
2022-09-08 23:11:39 +0530

    Create Permutations.java

---
+++ Recursion/Backtracking/Permutations.java
@@ -0,0 +1,22 @@
+public class Permutations {
+
+    public static void findPermutation(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        
+        for(int i=0;i<str.length();i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i)+str.substring(i+1);
+            findPermutation(newStr, ans+currChar);
+        }
+    }
+
+
+    public static void main(String args[]){
+        String str = "abc";
+        findPermutation(str, "");
+    }
+    
+}
